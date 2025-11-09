57d3596d737d18bb61bbb5e29f0e2a7539d35ce9
Herfando <herfando_91@yahoo.com>
2025-11-09 19:47:44 +0700

    completed navbar and hero

---
+++ src/components/container/03_About.tsx
@@ -0,0 +1,12 @@
+import { div } from 'framer-motion/client';
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <div>About</div>
+      <div>About</div>
+      <div>About</div>
+    </div>
+  );
+}

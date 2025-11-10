60781987faf24ab628d767d84a7669e9e1ded1af
Herfando <herfando_91@yahoo.com>
2025-11-10 10:12:37 +0700

    edit about

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Navbar from '../components/container/01_Navbar';
 import Home from '../components/container/02_Home';
+import About from '@/components/container/04_About';
+import Banner from '@/components/container/03_Banner';
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Home />
+      <Banner />
+      <About />
     </>
   );
 };

d9fd56a122e93d675088e9aeeaf97cbadbcfcc82
Herfando <herfando_91@yahoo.com>
2025-11-11 07:06:09 +0700

    create skewmotionprops

--- src/components/ui/skew-motion.tsx
+++ src/components/ui/skew-motion.tsx
@@ -2,3 +2,10 @@
 
 import { Slot } from '@radix-ui/react-slot';
 import { HTMLMotionProps, motion } from 'motion/react';
+
+interface SkewMotionProps extends HTMLMotionProps<'div'> {
+  children: React.ReactNode;
+  duration?: number;
+  delay?: number;
+  asChild?: boolean;
+}

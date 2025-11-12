1a8f5322f324075626796968225b9a7c5c2282de
Herfando <herfando_91@yahoo.com>
2025-11-12 15:10:29 +0700

    create skill

--- src/components/container/05_Skill.tsx
+++ src/components/container/05_Skill.tsx
@@ -9,7 +9,7 @@ export default function Skill() {
         <div className='flex gap-12'>
           <p className='dark:black grid h-[52.58] w-[52.58] place-items-center rounded-full bg-[#F5F5F5]'>
             <Image
-              src='/images/08_skill3.png'
+              src='/images/08_html.png'
               alt='html icon'
               width={23.33}
               height={23.33}
@@ -28,7 +28,83 @@ export default function Skill() {
         <div className='flex gap-12'>
           <p className='dark:black grid h-[52.58] w-[52.58] place-items-center rounded-full bg-[#F5F5F5]'>
             <Image
-              src='/images/08_skill3.png'
+              src='/images/06_css.png'
+              alt='html icon'
+              width={23.33}
+              height={23.33}
+              className='w-23.33 h-23.33'
+            />
+          </p>
+          <p className='self-center text-[18px] font-semibold'>HTML</p>
+        </div>
+        <p className='text-[rgba(65,70,81,1)] md:text-[16px]'>
+          Building the structure of web pages with semantic markup for
+          accessibility .
+        </p>
+      </div>
+      {/* 3.Javascript */}
+      <div className='space-y-8'>
+        <div className='flex gap-12'>
+          <p className='dark:black grid h-[52.58] w-[52.58] place-items-center rounded-full bg-[#F5F5F5]'>
+            <Image
+              src='/images/07_js.png'
+              alt='html icon'
+              width={23.33}
+              height={23.33}
+              className='w-23.33 h-23.33'
+            />
+          </p>
+          <p className='self-center text-[18px] font-semibold'>HTML</p>
+        </div>
+        <p className='text-[rgba(65,70,81,1)] md:text-[16px]'>
+          Building the structure of web pages with semantic markup for
+          accessibility .
+        </p>
+      </div>
+      {/* 4.React JS */}
+      <div className='space-y-8'>
+        <div className='flex gap-12'>
+          <p className='dark:black grid h-[52.58] w-[52.58] place-items-center rounded-full bg-[#F5F5F5]'>
+            <Image
+              src='/images/12_reactjs.png'
+              alt='html icon'
+              width={23.33}
+              height={23.33}
+              className='w-23.33 h-23.33'
+            />
+          </p>
+          <p className='self-center text-[18px] font-semibold'>HTML</p>
+        </div>
+        <p className='text-[rgba(65,70,81,1)] md:text-[16px]'>
+          Building the structure of web pages with semantic markup for
+          accessibility .
+        </p>
+      </div>
+      {/* 5.Mongo DB */}
+      <div className='space-y-8'>
+        <div className='flex gap-12'>
+          <p className='dark:black grid h-[52.58] w-[52.58] place-items-center rounded-full bg-[#F5F5F5]'>
+            <Image
+              src='/images/15_mongoDB.png'
+              alt='html icon'
+              width={23.33}
+              height={23.33}
+              className='w-23.33 h-23.33'
+            />
+          </p>
+          <p className='self-center text-[18px] font-semibold'>HTML</p>
+        </div>
+        <p className='text-[rgba(65,70,81,1)] md:text-[16px]'>
+          Building the structure of web pages with semantic markup for
+          accessibility .
+        </p>
+      </div>
+      {/* 6.Docker */}
+      <div className='space-y-8'>
+        <div className='flex gap-12'>
+          <p className='dark:black grid h-[52.58] w-[52.58] place-items-center rounded-full bg-[#F5F5F5]'>
+            <Image
+              src='/images/13_docker.png'
               alt='html icon'
               width={23.33}
               height={23.33}
